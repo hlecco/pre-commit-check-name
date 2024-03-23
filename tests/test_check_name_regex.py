c9a0aac9457d7292c9119a2c457ca0611c7ce957
Henrique Lecco <me@lecco.dev>
2024-03-22 21:00:04 -0300

    feat: create check-name-regex pre-commit hook

---
+++ tests/test_check_name_regex.py
@@ -0,0 +1,45 @@
+import pre_commit_hooks.check_name_regex as check_name_regex
+
+
+def test_check_all_match():
+    '''Assert every filename matches at least one regex.'''
+    filenames = ['foo', 'bar']
+    regex = ['^f', '^b']
+    dir_ = None
+
+    assert check_name_regex.check_name_regex(filenames, regex, dir_) == 0
+
+
+def test_check_number_unmatched():
+    '''Check if the number of unmatched filenames is correct.'''
+    filenames = ['foo', 'bar', 'baz']
+    regex = ['^f']
+    dir_ = None
+
+    assert check_name_regex.check_name_regex(filenames, regex, dir_) == 2
+
+
+def test_dir_filter():
+    '''Filter out filenames that are not on checked directories.'''
+    filenames = ['somedir/foo1', 'somedir/bar1', 'foo2', 'bar2']
+    regex = ['^f']
+    dir_ = ['somedir']
+
+    assert check_name_regex.check_name_regex(filenames, regex, dir_) == 1
+
+
+def test_argparse():
+    '''Check parameters usage on main call.'''
+    argv = [
+        '-r', '^f',
+        '-r', '^b',
+        'foo', 'bar'
+    ]
+    assert check_name_regex.main(argv) == 0
+
+    argv = [
+        '-r', '^f',
+        '-d', 'somedir',
+        'somedir/foo1', 'somedir/bar1', 'foo2', 'bar2'
+    ]
+    assert check_name_regex.main(argv) == 1
