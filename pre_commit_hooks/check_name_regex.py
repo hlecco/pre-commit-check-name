6a22f5ee2962d3fdc729b9c7891116e0615bf9d4
Henrique Lecco <me@lecco.dev>
2024-04-11 12:59:50 -0300

    feat: use pre-commit to run tests

--- pre_commit_hooks/check_name_regex.py
+++ pre_commit_hooks/check_name_regex.py
@@ -44,5 +44,5 @@ def main(argv=None):
     return check_name_regex(args.filenames, args.regex, args.dir)
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # pragma: nocover
     raise SystemExit(main())
