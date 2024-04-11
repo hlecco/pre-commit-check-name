43934dcc2a59e0aaa7aab30c28075cbe2f2f55b3
Henrique Lecco <me@lecco.dev>
2024-04-11 13:17:25 -0300

    feat: include code style requirements in pre-commit hook

--- pre_commit_hooks/check_name_regex.py
+++ pre_commit_hooks/check_name_regex.py
@@ -1,9 +1,28 @@
+"""Check file names against a regex pattern."""
 from pathlib import Path
 import argparse
 import re
 
 
 def check_name_regex(filenames, regex, dir_):
+    """Match a list of filenames agains a regex pattern.
+
+    Parameters
+    ----------
+    filenames: list
+        List of filenames (can be relative paths)
+    regex: str
+    dir_: list or None
+        If None, consider files from any directory.
+        If it is a list, only consider files in one of the directories
+        from the list.
+
+    Returns
+    -------
+    int
+        Number of files that don't match the requirements.
+
+    """
     if dir_ is not None:
         filenames = [
             f for f in filenames
@@ -30,6 +49,20 @@ def check_name_regex(filenames, regex, dir_):
 
 
 def main(argv=None):
+    """Parse CLI arguments and call check_name_regex.
+
+    Parameters
+    ----------
+    argv: list or None
+        List of command-line arguments.
+        If None, gets arguments from STDIN.
+
+    Returns
+    -------
+    int
+        Return from called check_name_regex function
+
+    """
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('filenames', nargs='*')
     parser.add_argument('-r', '--regex',
